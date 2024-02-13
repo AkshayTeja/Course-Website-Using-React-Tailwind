7d56f0171b0f906eb66c2a01e3189c7aa6e54ac4
Axay17 <akshayteja04@gmail.com>
2024-02-13 10:15:34 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Experts from "./components/Experts";
+import NewsLetter from "./components/NewsLetter";
 function App() {
   return (
     <>
     <Header/>
     <Banner/>
     <Experts/>
+    <NewsLetter/>
     </>
   );
 }
--- src/components/Experts.js
+++ src/components/Experts.js
@@ -2,13 +2,13 @@ import React from 'react'
 import laptop from '../assets/laptop.jpg'
 export default function Experts() {
   return (
-    <div className='max-w-[1240px] mx-auto my-10  grid grid-cols-2'>
-      <div className='border col-span-1 w-[80%]'>
+    <div className='max-w-[1240px] p-2 mx-auto my-10 md:grid grid-cols-2'>
+      <div className='col-span-1 md:w-[80%]'>
         <img src={laptop} />
       </div>
-      <div className='border col-span-1 flex flex-col justify-center'>
+      <div className='col-span-1 flex flex-col justify-center'>
         <h1 className='text-[#00df9a] font-bold my-2'>LEARN FROM EXPERTS</h1>
-        <p className='m-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat iure mollitia nam sint ut. Fuga corporis tempore quos autem, molestiae labore eos, consequatur vero rem vitae debitis expedita, facilis amet?</p>
+        <p className='my-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat iure mollitia nam sint ut. Fuga corporis tempore quos autem, molestiae labore eos, consequatur vero rem vitae debitis expedita, facilis amet?</p>
         <button className='w-[30%] bg-black inline text-white p-3 rounded'>Get Started</button>    
       </div>
     </div>
